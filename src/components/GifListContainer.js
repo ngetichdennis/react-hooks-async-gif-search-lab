dc1832ead8fd73253fef000cfd941d29212244e6
Dennis <ngetichdennis90@gmail.com>
2024-02-13 23:10:44 +0300

    completed assignment

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,35 @@
+
+import React, { useState, useEffect } from 'react';
+import GifList from './GifList';
+import GifSearch from './GifSearch';
+
+const GifListContainer = () => {
+  const [gifs, setGifs] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=pUl1GhYdY6707LiACNKivW1AtNawpwHl&rating=g&limit=3`
+      );
+      const result = await response.json();
+      setGifs(result.data);
+    };
+
+    fetchData();
+  }, [search]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearch(e.target.elements.search.value);
+  };
+
+  return (
+    <div>
+      <GifSearch handleSubmit={handleSubmit} />
+      <GifList gifs={gifs} />
+    </div>
+  );
+};
+
+export default GifListContainer;
